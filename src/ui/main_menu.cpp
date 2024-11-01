266c566203dee44d1c1a001b6a9de886cc8c3062
Vincent Bueno <vbueno@enssat.fr>
2024-11-01 15:00:07 +0100

    Init: Creating folder and files

---
+++ src/ui/main_menu.cpp
@@ -0,0 +1,5 @@
+//
+// Created by vince on 01/11/2024.
+//
+
+#include "main_menu.h"
---
+++ src/ui/main_menu.h
@@ -0,0 +1,9 @@
+//
+// Created by vince on 01/11/2024.
+//
+
+#ifndef MAIN_MENU_H
+#define MAIN_MENU_H
+
+#endif //MAIN_MENU_H
+
